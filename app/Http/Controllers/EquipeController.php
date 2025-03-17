73cd402dc07fcc85619f4dc4e74eb6bdad202423
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2025-03-16 21:21:57 -0300

    feat: controller para equipes

---
+++ app/Http/Controllers/EquipeController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EquipeController extends Controller
+{
+    public function index()
+    {
+        // listar equipes que o usuário é gestor
+    }
+
+    public function show()
+    {
+        // mostrar informações sobre as equipes
+    }
+
+    public function create()
+    {
+        // form para criar equipes
+    }
+
+    public function store()
+    {
+        // rota para armazenar equipe
+    }
+
+    public function update()
+    {
+        // rota para atualizar informações sobre equipes
+    }
+
+    public function edit()
+    {
+        // form para editar equipes
+    }
+
+    public function delete()
+    {
+        // deletar equipe
+    }
+}
