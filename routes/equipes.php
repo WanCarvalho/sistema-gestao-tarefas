46a45a56a9a986637373a20d42a5e9eb9ddcbd86
WanCarvalho <wandersoncarvalho.1501@gmail.com>
2025-03-18 21:54:32 -0300

    fix: corrigindo rotas para equipes

--- routes/equipes.php
+++ routes/equipes.php
@@ -12,12 +12,12 @@ Route::prefix('equipes')->controller(EquipeController::class)->group(function ()
     Route::delete('/{equipe:id}', 'delete')->name('equipes.delete');
 
     // Membro na equipe
-    Route::get('/equipes/{equipe}/membros/create', [EquipeController::class, 'createMembro'])->name('equipes.membros.create');
-    Route::post('/equipes/{equipe}/membros', [EquipeController::class, 'storeMembro'])->name('equipes.membros.store');
-    Route::delete('/equipes/{equipe}/membros/{membro}', [EquipeController::class, 'destroyMembro'])->name('equipes.membros.destroy');
+    Route::get('/{equipe}/membros/create', [EquipeController::class, 'createMembro'])->name('equipes.membros.create');
+    Route::post('/{equipe}/membros', [EquipeController::class, 'storeMembro'])->name('equipes.membros.store');
+    Route::delete('/{equipe}/membros/{membro}', [EquipeController::class, 'destroyMembro'])->name('equipes.membros.destroy');
 
     // Criar gestor na equipe
-    Route::get('/equipes/{equipe}/gestores/create', [EquipeController::class, 'createGestor'])->name('equipes.gestores.create');
-    Route::post('/equipes/{equipe}/gestores', [EquipeController::class, 'storeGestor'])->name('equipes.gestores.store');
-    Route::delete('/equipes/{equipe}/gestores/{gestor}', [EquipeController::class, 'destroyGestor'])->name('equipes.gestores.destroy');
+    Route::get('/{equipe}/gestores/create', [EquipeController::class, 'createGestor'])->name('equipes.gestores.create');
+    Route::post('/{equipe}/gestores', [EquipeController::class, 'storeGestor'])->name('equipes.gestores.store');
+    Route::delete('/{equipe}/gestores/{gestor}', [EquipeController::class, 'destroyGestor'])->name('equipes.gestores.destroy');
 });
